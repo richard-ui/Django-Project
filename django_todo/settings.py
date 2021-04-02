efc2bfc23965f683a3b1fc2abba74c50aafff16f
richard-ui <richard-jones96@hotmail.co.uk>
2021-04-02 11:04:57 +0000

    inital commit

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environment.get('SECRET_KEY, 0(n15np*u^y+$n)mz6=lpp3%7e@s1*xgr_2^*!rsshaxkspvc@')
+SECRET_KEY = os.environ.get('SECRET_KEY', '0(n15np*u^y+$n)mz6=lpp3%7e@s1*xgr_2^*!rsshaxkspvc@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
